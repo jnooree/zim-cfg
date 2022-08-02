ad60923972ab9a334b099b080629452d9080d0bb
jnooree <jnooree@snu.ac.kr>
2022-08-02 15:02:49 +0900

    Use zim's double-dot-expand instead aliases

--- init.zsh
+++ init.zsh
@@ -91,13 +91,6 @@ unset GREP_COLOR GREP_COLORS
 
 # Custom aliases
 alias -- -='cd -'
-
-alias -g ...=../..
-alias -g ....=../../..
-alias -g .....=../../../..
-alias -g ......=../../../../..
-alias -g .......=../../../../../..
-
 alias 1='cd -1'
 alias 2='cd -2'
 alias 3='cd -3'
